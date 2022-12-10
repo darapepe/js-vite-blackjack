7e73fbf066ac2225b45987296668c0918a2efad9
Darwin Perez Perez <perez.darwin@gmail.com>
2022-12-10 13:40:41 -0500

    Primer commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * @param {array<String>} deck 
+ * @returns {String} nos permite tomar una carta del deck
+ */
+export const pedirCarta = (deck) => {
+
+    if (!deck || deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
